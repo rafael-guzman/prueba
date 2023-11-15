dce7e591e245149e5320b55b9b50266c67ce6143
Rafael Guzmán Valverde <150675149+rafael-guzman@users.noreply.github.com>
2023-11-15 11:05:35 +0100

    Script para iniciar el build

---
+++ startAutoBuild.sh
@@ -0,0 +1,7 @@
+echo Building proyect
+
+echo Starting Build Process
+
+'/c/Program Files/Unity/Hub/Editor/2021.3.11f1/Editor/Unity.exe' -force-free -quit -batchmode -projectPath Proyectos/prueba -executeMethod BuildScript.PerformBuild albolote 
+
+echo Build terminated
